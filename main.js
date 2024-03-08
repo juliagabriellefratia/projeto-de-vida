0aab7b71c5f85d0a43d5e645417ef98dd8861753
Julia <107054220+juliagabriellefratia@users.noreply.github.com>
2024-03-08 08:45:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,20 +15,13 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function atualizaCronometro(){
-for (let i = 0; i < contadores.length; i++) {
-    contadores[i].textContent = calculaTempo(tempos[i]);
-}
-}
-
-atualizaCronometro();
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -41,6 +34,21 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+    if (tempoFinal > 0){ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else {
+    return "Prazo Finalizado";
+}
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-  }
+}
+
+  function atualizaCronometro(){
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+        }
+    }
+    
+    function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000); }
+
+//comecaCronometro();
